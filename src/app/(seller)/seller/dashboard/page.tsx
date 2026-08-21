f73944544790e71f6f6ff4b0a7323b50862e3dc7
Viniciuseudes <vinicius.egf13@gmail.com>
2026-08-21 13:44:54 -0300

    ajuste de dash do vendedor

---
+++ src/app/(seller)/seller/dashboard/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { VendorPedidos } from "@/components/seller/VendorPedidos";
+
+export default function SellerDashboardPage() {
+  return (
+    <div className="w-full">
+      <VendorPedidos />
+    </div>
+  );
+}
